7b046042b17885cce6123690102ced8a7c4c082d
pabloins <53983994+pabloins@users.noreply.github.com>
2024-04-19 19:01:24 -0400

    init proyect

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Providers from './Providers';
+
+const App: React.FC = () => {
+    return (
+        <Providers>
+            <h1>React Typescript</h1>
+        </Providers>
+    );
+}
+
+export default App;
---
+++ src/views/Pokedex.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Pokedex: React.FC =  () => <h1>React</h1>
+
+export default Pokedex;
