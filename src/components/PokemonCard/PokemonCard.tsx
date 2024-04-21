9fe9e6a2fc359dcc0375ec37062bd25016e68476
pabloins <53983994+pabloins@users.noreply.github.com>
2024-04-20 20:31:45 -0400

    new button, info pokemon

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -2,8 +2,10 @@ import React, { useMemo } from "react";
 import { useGetPokemon } from "../../hooks/useGetPokemon";
 import { PokemonListItem } from "../../interfaces/PokemonListItem";
 import { getMainPokemonType } from "../../utils/getMainPokemonType";
-import { Label } from "../../shared/Label/Label";
+import { Label } from "../shared/Label/Label";
 import { capitilizeFirstLetter } from "../../utils/capitilizeFirstLetter";
+import { FavoriteButton } from "../shared/Button/FavoriteButton";
+import { useNavigate } from "react-router";
 
 interface PokemonCardProps {
     pokemon?: PokemonListItem;
@@ -13,11 +15,17 @@ interface PokemonCardProps {
 export const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon, pokemonId }) => {
     const { pokemonData } = useGetPokemon(pokemon?.name, pokemonId);
     const mainType = useMemo(() => pokemonData && getMainPokemonType(pokemonData), [pokemonData])
+    const navigate = useNavigate();
+
+    const onClick = () => {
+        navigate(`/pokemon/${pokemonData?.name}`);
+    }
 
     return (
         <>
-            <div className={`${mainType}-background w-56 h-56 rounded-lg shadow-lg p-4`}>
-                <div className="flex flex-col items-center mx-auto">
+            <div className={`${mainType}-background relative w-56 h-56 rounded-lg shadow-lg p-4 cursor-pointer`}>
+                <FavoriteButton pokemonId={pokemonData?.id ?? 0}/>
+                <div className="flex flex-col items-center mx-auto" onClick={onClick}>
                     <Label>{pokemonData?.name ? capitilizeFirstLetter(pokemonData.name) : ""}</Label>
                     <img
                         className="mx-auto w-40 h-40"
---
+++ src/components/PokemonInfo/PokemonInfo.tsx
@@ -0,0 +1,33 @@
+import React, { useMemo } from "react";
+import { useGetPokemon } from "../../hooks/useGetPokemon";
+import { useParams } from "react-router";
+import { getMainPokemonType } from "../../utils/getMainPokemonType";
+import { capitilizeFirstLetter } from "../../utils/capitilizeFirstLetter";
+import { convertLbsToKg } from "../../utils/convertLbsToKg";
+import { convertInchesToCm } from "../../utils/convertInchesToCm";
+import { PokemonSprites } from "../PokemonSprites/PokemonSprites";
+
+export const PokemonInfo = () => {
+    const { pokemonName } = useParams();
+    const { pokemonData } = useGetPokemon(pokemonName);
+    const mainType = useMemo(() => pokemonData && getMainPokemonType(pokemonData), [pokemonData]);
+
+    return (
+        <>
+            <div className="flex flex-row justify-between shadow-lg bg-gray-100 rounded-lg">
+                <div className={`${mainType}-background w-72 h-72 rounded-l-lg items-center`}>
+                    <img src={pokemonData?.sprites?.front_default}
+                        alt={pokemonData?.name ?? ""} className="mx-auto w-72 h-72" />
+                </div>
+                <div className="flex flex-col grow p-5 gap-3">
+                    <h1 className="text-3xl">
+                        {capitilizeFirstLetter(pokemonData?.name ?? "")}
+                    </h1>
+                    <span>{`Weight: ${convertLbsToKg(pokemonData?.weight ?? 0)} kg`}</span>
+                    <span>{`Height: ${convertInchesToCm(pokemonData?.height ?? 0)} cm`}</span>
+                    <PokemonSprites pokemonName={pokemonName} />
+                </div>
+            </div>
+        </>
+    )
+}
