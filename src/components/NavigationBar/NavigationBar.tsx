7e229293196816566dcc8eb1b7bc86b0968a6dbb
pabloins <53983994+pabloins@users.noreply.github.com>
2024-04-21 22:15:15 -0400

    desarrollo de navbar

---
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import pokeball from "../../assets/pokeball.png";
+
+const NavigationBar: React.FC = () => (
+    <nav className="mx-auto bg-yellow-400 flex justify-between h-12 items-center shadow-lg">
+        <div className="mx-auto flex justify-between items-center w-9/12">
+            <Link to="/">
+                <img src={pokeball} alt="Poke Logo" className="w-10 h-10"/>
+            </Link>
+            <div className="flex gap-5 items-center">
+                <Link className="text-black hover:text-white" to="/">Pokedex</Link>
+                <Link className="text-black hover:text-white" to="/favorite">Favorite</Link>
+            </div>
+        </div>
+    </nav>
+)
+
+export default NavigationBar;
