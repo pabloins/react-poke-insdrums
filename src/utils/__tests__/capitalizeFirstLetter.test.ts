a6d87bf6d7b7bae09cf629921b9532c44f8bc2b5
pabloins <53983994+pabloins@users.noreply.github.com>
2024-04-23 17:37:27 -0400

    new unit test

--- src/utils/__tests__/capitalizeFirstLetter.test.ts
+++ src/utils/__tests__/capitalizeFirstLetter.test.ts
@@ -8,4 +8,8 @@ describe("capitilizeFirstLetter", () => {
     it("should return empty string if empty string is passed", () => {
         expect(capitilizeFirstLetter("")).toBe("")
     })
+
+    it("should return the same string if the first letter is already capitalized", () => {
+        expect(capitilizeFirstLetter("Hello")).toBe("Hello")
+    })
 })
